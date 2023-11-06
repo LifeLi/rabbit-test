dd47764020027d5109d3cd002ce1b86876c15581
Sergey Pozhidaev <pozhidaev_94@mail.ru>
2023-11-06 22:53:12 +0300

    - send message in rabbit

--- request/producer.php
+++ request/producer.php
@@ -1,2 +1,20 @@
 <?php
-echo 'Hello world!';
+require_once __DIR__ . '/../vendor/autoload.php';
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+use PhpAmqpLib\Message\AMQPMessage;
+
+$connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
+$channel = $connection->channel();
+
+$channel->queue_declare('hello', false, false, false, false);
+
+$string = 'Hello World!';
+$msg = new AMQPMessage($string);
+$channel->basic_publish($msg, '', 'hello');
+
+$channel->close();
+$connection->close();
+
+echo '<pre>';
+//print_r($channel);
+echo '</pre>';
